1ccee30f06a40aadee63322dda8b155723174969
Viktoria Loe <viktoria.loe@soprasteria.com>
2024-07-16 13:34:46 +0200

    linted

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -15,7 +15,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("daisyui")
+  ],
   daisyui: {
     themes: ["light", "dark", "retro", "aqua", "valentine", "cyberpunk"],
   },
